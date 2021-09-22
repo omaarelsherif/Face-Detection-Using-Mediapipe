c4c10ea208c224dc4997677872acb880ad80a8fb
Omar Elsherif <35617008+omaarelsherif@users.noreply.github.com>
2021-09-22 22:13:51 +0200

    Update project code

--- face_detection_mediapipe.py
+++ face_detection_mediapipe.py
@@ -16,73 +16,73 @@ mp_face_drawing   = mp.solutions.drawing_utils
 # Detect faces on an image
 def detectFromImg(image_path):
 	"""Function to detect faces on an image"""
- 
-	# Load the images
-	image = cv2.imread(image_path)
 
 	# Using mediapipe face detector
 	with mp_face_detection.FaceDetection(min_detection_confidence=0.5) as face_detection:
+		
+		# Load the images
+		img = cv2.imread(image_path)
     
 		# Convert the BGR image to RGB
-		image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-		image.flags.writeable = False
+		img_grey = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 		# Get faces on the an image (frame)
-		faces = face_detection.process(image)
+		faces = face_detection.process(img_grey)
+
+		# Convert image back to its colors for displaying
+		img_final = cv2.cvtColor(img_grey, cv2.COLOR_RGB2BGR)
 
 		# Draw the face detection annotations on the image
-		image.flags.writeable = True
-		image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 		if faces.detections:
 			for detection in faces.detections:
-				mp_face_drawing.draw_detection(image, detection)
+				mp_face_drawing.draw_detection(img_final, detection)
 		
-		# Show repeted images (Video)
-		cv2.imshow('Face Detection', image)
+		# Show repeted frames (Video)
+		cv2.imshow('Face Detection Mediapipe', img_final)
 		cv2.waitKey(0)
 
 # Detect faces on video or cam
 def detectFromVid(video_path=None, cam=False):
 	"""Function to detect faces on a video or cam"""
 
-	# Load the video or cam based on cam flag
-	if cam:
-		video = cv2.VideoCapture(0)
-	else:
-		video = cv2.VideoCapture(video_path)
-
 	# Using mediapipe face detector
 	with mp_face_detection.FaceDetection(min_detection_confidence=0.5) as face_detection:
+     
+		# Load the video or cam based on cam flag
+		if cam:
+			video = cv2.VideoCapture(0)
+		else:
+			video = cv2.VideoCapture(video_path)
 
 		# Loop over video frames
-	    while True:
+		while True:
          
             # Get every frame on the video
-	        success, image = video.read()
-	
-		    # If there's no more frames, stop 
-	        if not success:
-		        break
-    
-	        # Convert the BGR image to RGB
-	        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-	        image.flags.writeable = False
+			success, frame = video.read()
 
-	        # Get faces on the an image (frame)
-	        faces = face_detection.process(image)
+			# If there's no more frames, stop 
+			if not success:
+				break
 
-	        # Draw the face detection annotations on the image
-	        image.flags.writeable = True
-	        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-	        if faces.detections:
-	          for detection in faces.detections:
-	            mp_face_drawing.draw_detection(image, detection)
-		  
-		    # Show repeted images (Video)
-	        cv2.imshow('Face Detection', image)
-	        if cv2.waitKey(5) & 0xFF == 27:
-	          break
-     
+			# Convert the BGR frame to RGB
+			frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+			# Get faces on the an image (frame)
+			faces = face_detection.process(frame)
+
+			# Convert the frame back to its colors for displaying
+			frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+   
+			# Draw the face detection annotations on the image
+			if faces.detections:
+				for detection in faces.detections:
+					mp_face_drawing.draw_detection(frame, detection)
+			
+			# Show repeted images (Video)
+			cv2.imshow('Face Detection Mediapipe', frame)
+			if cv2.waitKey(5) & 0xFF == 27:
+				break
+		
     	# End 
 	video.release()
 
